6b896295fd19b4b7e91b4cd3f05297188be1944b
dickongray <dickongray@gmail.com>
2016-02-16 15:09:11 +1300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//var app = require('express')();
+//var http = require('http').Server(app);
+//old display showing hello world
+//app.get('/', function(req, res){
+	//res.send('<h1>Hello World</h1>');
+//});
+//new this is looking for index.html
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+//old
+//io.on('connection', function(socket){
+//	console.log('a user connected');
+//});
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
